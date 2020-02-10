f09e299942093cb157172a078609cdd25d57b67f
Opeyemi is <opeyemib39@gmail.com>
2020-02-10 12:34:23 +0100

    erc-20 files added

--- day1-building ethereum based ERC20 Token/Token.sol
+++ day1-building ethereum based ERC20 Token/Token.sol
@@ -1,1 +1,19 @@
 pragma solidity ^0.5.3;
+contract NgnToken {
+  string public name = "Ngn Token";
+  string public symbol = "NGN";
+  string public standard = "Ngn Token v0.1";
+  unint256 public totalSupply;
+
+  event Transfer(
+    address indexed _from;
+    address indexed _to;
+    uint256 _value
+    );
+
+    event Approval(
+      address indexed _owner,
+      address indexed _spender,
+      uint256 _value,
+      );
+}
